0d969b4b2ed7dfac0c39479c16ddd1bc6dc8044a
Chris Cotter <ccotter@utexas.edu>
2013-03-19 21:07:27 -0500

    barely functional

--- strava.js
+++ strava.js
@@ -13,9 +13,9 @@ var others = $(".details > a");
 var n = parseInt(others.get()[0].innerHTML.split(" ")[0]);
 others.get()[0].innerHTML = (n+10)  + " others";
 
+//$("body").bind("DOMNodeInserted", function() {
 others.bind("click", function() {
-    console.log("Here");
-    var node = $("body div.ui-dialog ul").get();
+    var node = $("body div.ui-dialog ul")[0];
     console.log(node.innerHTML);
     for (var i = 0; i < 10; ++i)
         node.innerHTML += Mike;
